04d95c4e262bb47faf3e644fa7d47fac716039a4
S4L4H77 <salahamleh45@gmail.com>
2026-07-19 02:19:48 +0300

    add the themes and light and dark theme for the pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:expensetracker/screens/home_screen.dart';
+import 'package:expensetracker/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-// import 'package:expensetracker/screens/home_screen.dart';
-// import 'package:expensetracker/screens/detail_screen.dart';
-import 'package:expensetracker/screens/empty_state_screen.dart';
+import 'package:expensetracker/screens/home_screen.dart';
+import 'package:expensetracker/screens/detail_screen.dart';
+import 'package:expensetracker/widgets/empty_state.dart';
+import 'package:expensetracker/core/theme.dart';
 
 void main() {
   runApp(
-    MaterialApp(home: EmptyStateScreen(), debugShowCheckedModeBanner: false),
+    MaterialApp(
+      theme: AppTheme.light, // used in light mode
+      darkTheme: AppTheme.dark, // used in dark mode
+      themeMode: ThemeMode.system, // follows the phone setting
+      home: const DetailScreen(),
+      debugShowCheckedModeBanner: false,
+    ),
   );
 }
