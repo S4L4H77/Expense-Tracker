1690a7e1e091abfaa2336f5e5bdffcd40641e524
S4L4H77 <salahamleh45@gmail.com>
2026-07-18 16:53:01 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:expensetracker/screens/Home.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomeScreen(), debugShowCheckedModeBanner: false));
+}
